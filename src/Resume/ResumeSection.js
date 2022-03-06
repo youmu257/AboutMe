299c43305de09043014f0104def1c212d5ce0018
youmu257 <youmu257@gmail.com>
2022-03-06 20:23:30 +0800

    feat: 新增工作經歷

--- src/Resume/ResumeSection.js
+++ src/Resume/ResumeSection.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Education from './Education'
+import WorkExperience from './WorkExperience'
 
 function Resume () {
   return (
     <section id="resume">
       <Education />
+      <WorkExperience />
     </section>
   )
 }
